3757c0b233a52219348a84905a1ee38b85c57192
Musharraf23 <musharrafnice@gmail.com>
2022-08-27 12:24:03 +0530

    minor changes

---
+++ diff.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+    int number1, number2, diff;
+
+    diff = number1 - number2;
+
+    printf("%d - %d = %d", number1, number2, diff);
+    return 0;
+}
--- sum.c
+++ sum.c
@@ -6,6 +6,6 @@ int main()
 
     sum = number1 + number2;
 
-    printf("%d + %d = %d", number1, number2, sum);
+    printf(" this is %d + %d = %d", number1, number2, sum);
     return 0;
 }
